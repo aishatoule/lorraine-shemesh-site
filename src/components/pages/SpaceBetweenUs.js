a41ea92bac78297efbbcb82a7787df7fca35565b
Aisha Toulegenova <aishatoulegenova@MacBook-Pro.domain>
2020-02-25 23:30:24 -0800

    adding image grid

--- src/components/pages/SpaceBetweenUs.js
+++ src/components/pages/SpaceBetweenUs.js
@@ -1,42 +1,45 @@
 import React, { Component } from 'react';
 import HomeImage from '../../images/01a. Accordion painting.jpg';
+import tsbu2 from '../../images/01b. Accordion detail (head).jpeg';
+import tsbu3 from '../../images/02a. Attached painting.jpg';
+import tsbu4 from '../../images/03. Sway painting.jpg';
+import tsbu5 from '../../images/04c. Hinge detail (feet).jpg';
 
 class SpaceBetweenUs extends Component {
 
     render() {
-        const images = 
-        [{
-            details: "Accordion \n 2018, 65 x 55 in., o/c ",
-            src: HomeImage
-        },
-        {
-            details: "Accordion \n 2018, 65 x 55 in., o/c ",
-            src: HomeImage
-        },{
-            details: "Accordion \n 2018, 65 x 55 in., o/c ",
-            src: HomeImage
-        },{
-            details: "Accordion \n 2018, 65 x 55 in., o/c ",
-            src: HomeImage
-        },{
-            details: "Accordion \n 2018, 65 x 55 in., o/c ",
-            src: HomeImage
-        },
-        ]
-
-        const mappedImages = images.map((image, index) => {
-            return (
-                <div key={index} className="grid-image-container">
-                    <img src={image.src} className="grid-image" alt="The Space Between Us exhibition"/>
-                    {/* <p>{image.details}</p> */}
-                </div>
-            )
-        })
         return (
             <div>
                 <h3 className="page-content-title">The Space Between Us</h3>
                 <div className="image-grid-container">
-                    {mappedImages}
+                    <div className="column">
+                        <img src={HomeImage} className="grid-image" alt="The Space Between Us exhibition"/>
+                        <img src={tsbu2} className="grid-image" alt="The Space Between Us exhibition"/>
+                        <img src={tsbu3} className="grid-image" alt="The Space Between Us exhibition"/>
+                        <img src={tsbu4} className="grid-image" alt="The Space Between Us exhibition"/>
+                        <img src={tsbu5} className="grid-image" alt="The Space Between Us exhibition"/>
+                    </div>
+                    <div className="column">
+                        <img src={tsbu4} className="grid-image" alt="The Space Between Us exhibition"/>
+                        <img src={tsbu3} className="grid-image" alt="The Space Between Us exhibition"/>
+                        <img src={tsbu5} className="grid-image" alt="The Space Between Us exhibition"/>
+                        <img src={tsbu2} className="grid-image" alt="The Space Between Us exhibition"/>
+                        <img src={tsbu4} className="grid-image" alt="The Space Between Us exhibition"/>
+                    </div>
+                    <div className="column">
+                        <img src={HomeImage} className="grid-image" alt="The Space Between Us exhibition"/>
+                        <img src={tsbu2} className="grid-image" alt="The Space Between Us exhibition"/>
+                        <img src={tsbu3} className="grid-image" alt="The Space Between Us exhibition"/>
+                        <img src={tsbu4} className="grid-image" alt="The Space Between Us exhibition"/>
+                        <img src={tsbu5} className="grid-image" alt="The Space Between Us exhibition"/>
+                    </div>
+                    <div className="column">
+                        <img src={tsbu2} className="grid-image" alt="The Space Between Us exhibition"/>
+                        <img src={tsbu3} className="grid-image" alt="The Space Between Us exhibition"/>
+                        <img src={tsbu5} className="grid-image" alt="The Space Between Us exhibition"/>
+                        <img src={tsbu2} className="grid-image" alt="The Space Between Us exhibition"/>
+                        <img src={tsbu4} className="grid-image" alt="The Space Between Us exhibition"/>
+                    </div>
                 </div>
             </div>
         )
