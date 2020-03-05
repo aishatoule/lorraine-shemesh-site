4bee972ac2623d55beb411117b4a3b8d457187f1
Aisha Toulegenova <aishatoulegenova@MacBook-Pro.domain>
2020-03-04 21:27:49 -0800

    fixing bug where mobile logo opens menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,15 +5,28 @@ import Burger from './navigation/Burger';
 
 const Navigation = () => {
     const [open, setOpen] = useState(false); 
+    const isMobile = window.innerWidth < 768;
+
+    var lorraineNameLogo = isMobile 
+    ? 
+    <div className="lorraine-shemesh-logo">
+        <Link to="/">
+            <h2 className="uppercase"><b>Lorraine</b></h2>
+            <h2 className="uppercase"><b>Shemesh</b></h2> 
+        </Link>
+    </div>
+    :
+    <div className="lorraine-shemesh-logo">
+        <Link to="/" onClick={() => setOpen(!open)}>
+            <h2 className="uppercase"><b>Lorraine</b></h2>
+            <h2 className="uppercase"><b>Shemesh</b></h2> 
+        </Link>
+    </div>
+
     return (
         <div className="navigation-menu">
             <div className="navigation-header">
-                <div className="lorraine-shemesh-logo">
-                    <Link to="/" onClick={() => setOpen(!open)}>
-                    <h2 className="uppercase"><b>Lorraine</b></h2>
-                    <h2 className="uppercase"><b>Shemesh</b></h2> 
-                    </Link>
-                </div>
+                {lorraineNameLogo}
                 <div className="hide-on-desktop">
                     <Burger open={open} setOpen={setOpen}/>
                 </div>
