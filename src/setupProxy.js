4dc8f309e9929e8a3f3c289ac85395a1454fd350
tillmane <61099006+tillmane@users.noreply.github.com>
2020-04-14 21:09:22 -0700

    Heroku proxy bug fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,6 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    proxy({ target: "http://localhost:5000" })
+    proxy({ target: "https://infinite-island-44222.herokuapp.com" })
   );
 };
