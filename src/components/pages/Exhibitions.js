be856f8d9badf35398d69e4197fdcabf7673af2a
Aisha Toulegenova <aishatoulegenova@Aishas-MBP.domain>
2020-04-20 00:47:59 -0700

    exhibition page

--- src/components/pages/Exhibitions.js
+++ src/components/pages/Exhibitions.js
@@ -1,9 +1,173 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Exhibition extends Component {
-    render() {
-        return <div>Exhibition</div>
-    }
+const Exhibitions = () => {
+    return (
+        <div>
+            <div className="exhibition-section-type">
+                <p className="exhibition-type-title"><b>Solo Exhibitions</b></p>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>2019</b></p>
+                    <p className="exhibition-name">The Space Between Us</p>
+                    <p className="exhibition-gallery-name"><b>Gerald Peters Gallery</b>, New York, New York</p>
+                    <p className="exhibition-dates">September 12 — October 16, 2019</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>2016</b></p>
+                    <p className="exhibition-name">Inside Out</p>
+                    <p className="exhibition-gallery-name"><b>Rhode Island School of Design</b>, Providence, Rhode Island</p>
+                    <p className="exhibition-dates">May 7 — June 10, 2016</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>2009</b></p>
+                    <p className="exhibition-name">Intersections – Paintings</p>
+                    <p className="exhibition-gallery-name"><b>Allan Stone Gallery</b>, New York, New York</p>
+                    <p className="exhibition-dates">February 28 - April 18, 2009</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>2006</b></p>
+                    <p className="exhibition-name">Breaking the Surface</p>
+                    <p className="exhibition-gallery-name"><b>Butler Institute of American Art</b>, Youngstown, Ohio</p>
+                    <p className="exhibition-dates">February 26 — April 9, 2006</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>2004</b></p>
+                    <p className="exhibition-name">Liquid States – Paintings and Drawings</p>
+                    <p className="exhibition-gallery-name"><b>Allan Stone Gallery</b>, New York, New York</p>
+                    <p className="exhibition-dates">May 1 - June 11, 2004</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>2009</b></p>
+                    <p className="exhibition-name">Intersections – Paintings</p>
+                    <p className="exhibition-gallery-name"><b>Allan Stone Gallery</b>, New York, New York</p>
+                    <p className="exhibition-dates">February 28 - April 18, 2009</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>2000</b></p>
+                    <p className="exhibition-name">Water-Works</p>
+                    <p className="exhibition-gallery-name"><b>Allan Stone Gallery</b>, New York, New York</p>
+                    <p className="exhibition-dates">April 29 - May 27, 1995</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>1991</b></p>
+                    <p className="exhibition-name">Recent Paintings</p>
+                    <p className="exhibition-gallery-name"><b>Allan Stone Gallery</b>, New York, New York</p>
+                    <p className="exhibition-dates">April 6 - May 4, 1991</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>1988</b></p>
+                    <p className="exhibition-name">Paintings, Drawings and Quilts</p>
+                    <p className="exhibition-gallery-name"><b>Allan Stone Gallery</b>, New York, New York</p>
+                    <p className="exhibition-dates">March 5 - 31, 1988</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>1985</b></p>
+                    <p className="exhibition-name">Quilts and Drawings</p>
+                    <p className="exhibition-gallery-name"><b>Allan Stone Gallery</b>, New York, New York</p>
+                    <p className="exhibition-dates">June 4 - July 3, 1985</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>1983-84</b></p>
+                    <p className="exhibition-name">Paintings, Drawings and Quilts</p>
+                    <p className="exhibition-gallery-name"><b>Allan Stone Gallery</b>, New York, New York</p>
+                    <p className="exhibition-dates">December 3, 1983 - January 5, 1984</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>1978</b></p>
+                    <p className="exhibition-name">Paintings and Watercolors</p>
+                    <p className="exhibition-gallery-name"><b>Alpha Gallery</b>, Boston, Massachusetts</p>
+                    <p className="exhibition-dates">December 3 - 31,1978</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>1976</b></p>
+                    <p className="exhibition-name">Paintings and Drawings</p>
+                    <p className="exhibition-gallery-name"><b>Rhode Island School of Design</b>, Providence, Rhode Island</p>
+                    <p className="exhibition-dates">January 21 - 27, 1976</p>
+                </div>
+            </div>
+
+            <div className="exhibition-section-type">
+                <p className="exhibition-type-title"><b>Selected Group Exhibitions</b></p>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>2019</b></p>
+                    <p className="exhibition-name">The Space Between Us</p>
+                    <p className="exhibition-gallery-name"><b>Gerald Peters Gallery</b>, New York, New York</p>
+                    <p className="exhibition-dates">September 12 — October 16, 2019</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>2016</b></p>
+                    <p className="exhibition-name">Inside Out</p>
+                    <p className="exhibition-gallery-name"><b>Rhode Island School of Design</b>, Providence, Rhode Island</p>
+                    <p className="exhibition-dates">May 7 — June 10, 2016</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>2009</b></p>
+                    <p className="exhibition-name">Intersections – Paintings</p>
+                    <p className="exhibition-gallery-name"><b>Allan Stone Gallery</b>, New York, New York</p>
+                    <p className="exhibition-dates">February 28 - April 18, 2009</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>2006</b></p>
+                    <p className="exhibition-name">Breaking the Surface</p>
+                    <p className="exhibition-gallery-name"><b>Butler Institute of American Art</b>, Youngstown, Ohio</p>
+                    <p className="exhibition-dates">February 26 — April 9, 2006</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>2004</b></p>
+                    <p className="exhibition-name">Liquid States – Paintings and Drawings</p>
+                    <p className="exhibition-gallery-name"><b>Allan Stone Gallery</b>, New York, New York</p>
+                    <p className="exhibition-dates">May 1 - June 11, 2004</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>2009</b></p>
+                    <p className="exhibition-name">Intersections – Paintings</p>
+                    <p className="exhibition-gallery-name"><b>Allan Stone Gallery</b>, New York, New York</p>
+                    <p className="exhibition-dates">February 28 - April 18, 2009</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>2000</b></p>
+                    <p className="exhibition-name">Water-Works</p>
+                    <p className="exhibition-gallery-name"><b>Allan Stone Gallery</b>, New York, New York</p>
+                    <p className="exhibition-dates">April 29 - May 27, 1995</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>1991</b></p>
+                    <p className="exhibition-name">Recent Paintings</p>
+                    <p className="exhibition-gallery-name"><b>Allan Stone Gallery</b>, New York, New York</p>
+                    <p className="exhibition-dates">April 6 - May 4, 1991</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>1988</b></p>
+                    <p className="exhibition-name">Paintings, Drawings and Quilts</p>
+                    <p className="exhibition-gallery-name"><b>Allan Stone Gallery</b>, New York, New York</p>
+                    <p className="exhibition-dates">March 5 - 31, 1988</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>1985</b></p>
+                    <p className="exhibition-name">Quilts and Drawings</p>
+                    <p className="exhibition-gallery-name"><b>Allan Stone Gallery</b>, New York, New York</p>
+                    <p className="exhibition-dates">June 4 - July 3, 1985</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>1983-84</b></p>
+                    <p className="exhibition-name">Paintings, Drawings and Quilts</p>
+                    <p className="exhibition-gallery-name"><b>Allan Stone Gallery</b>, New York, New York</p>
+                    <p className="exhibition-dates">December 3, 1983 - January 5, 1984</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>1978</b></p>
+                    <p className="exhibition-name">Paintings and Watercolors</p>
+                    <p className="exhibition-gallery-name"><b>Alpha Gallery</b>, Boston, Massachusetts</p>
+                    <p className="exhibition-dates">December 3 - 31,1978</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>1976</b></p>
+                    <p className="exhibition-name">Paintings and Drawings</p>
+                    <p className="exhibition-gallery-name"><b>Rhode Island School of Design</b>, Providence, Rhode Island</p>
+                    <p className="exhibition-dates">January 21 - 27, 1976</p>
+                </div>
+            </div>
+
+        </div>)
 }
 
-export default Exhibition;
+export default Exhibitions;
