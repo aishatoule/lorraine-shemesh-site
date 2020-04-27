fffbeaac7aa78cf16021a4f1fd511a37b02f2847
Aisha Toulegenova <aishatoulegenova@Aishas-MBP.domain>
2020-04-26 21:59:00 -0700

    exhibition page

--- src/components/pages/Exhibitions.js
+++ src/components/pages/Exhibitions.js
@@ -88,40 +88,50 @@ const Exhibitions = () => {
             <div className="exhibition-section-type">
                 <p className="exhibition-type-title"><b>Selected Group Exhibitions</b></p>
                 <div className="exhibition-container">
+                    <p className="exhibition-year"><b>2020</b></p>
+                    <p className="exhibition-name">Abstract Allegories</p>
+                    <p className="exhibition-gallery-name"><b>Spanierman Modern</b>, Miami Beach, Florida</p>
+                    <p className="exhibition-dates">January 6 - February 8, 2020</p>
+                </div>
+                <div className="exhibition-container">
                     <p className="exhibition-year"><b>2019</b></p>
-                    <p className="exhibition-name">The Space Between Us</p>
+                    <p className="exhibition-name">Art on Paper</p>
+                    <p className="exhibition-gallery-name"><b>Spanierman Modern</b>, New York, New York</p>
+                    <p className="exhibition-dates">March 7 - 10, 2019</p>
+                </div>
+                <div className="exhibition-container">
+                    <p className="exhibition-year"><b>2018</b></p>
+                    <p className="exhibition-name">Women: Summer</p>
                     <p className="exhibition-gallery-name"><b>Gerald Peters Gallery</b>, New York, New York</p>
-                    <p className="exhibition-dates">September 12 — October 16, 2019</p>
+                    <p className="exhibition-dates">June 21 - August 24, 2018</p>
                 </div>
                 <div className="exhibition-container">
-                    <p className="exhibition-year"><b>2016</b></p>
-                    <p className="exhibition-name">Inside Out</p>
-                    <p className="exhibition-gallery-name"><b>Rhode Island School of Design</b>, Providence, Rhode Island</p>
-                    <p className="exhibition-dates">May 7 — June 10, 2016</p>
+                    <p className="exhibition-year"><b>2017-18</b></p>
+                    <p className="exhibition-name">Fool The Eye</p>
+                    <p className="exhibition-gallery-name"><b>The Nassau County Museum of Art</b>, Arnold & Joan Saltzman Fine Art Building, Roslyn Harbor, New York</p>
+                    <p className="exhibition-dates">November 18, 2017 - March 4, 2018</p>
                 </div>
                 <div className="exhibition-container">
-                    <p className="exhibition-year"><b>2009</b></p>
-                    <p className="exhibition-name">Intersections – Paintings</p>
-                    <p className="exhibition-gallery-name"><b>Allan Stone Gallery</b>, New York, New York</p>
-                    <p className="exhibition-dates">February 28 - April 18, 2009</p>
+                    <p className="exhibition-year"><b>2017</b></p>
+                    <p className="exhibition-name">Art Miami</p>
+                    <p className="exhibition-gallery-name"><b>Gerald Peters Gallery & Spanierman Modern</b>, Miami, Florida</p>
+                    <p className="exhibition-dates">December 5 - 10, 2017</p>
                 </div>
                 <div className="exhibition-container">
-                    <p className="exhibition-year"><b>2006</b></p>
-                    <p className="exhibition-name">Breaking the Surface</p>
-                    <p className="exhibition-gallery-name"><b>Butler Institute of American Art</b>, Youngstown, Ohio</p>
-                    <p className="exhibition-dates">February 26 — April 9, 2006</p>
+                    <p className="exhibition-name">Art Market San Francisco</p>
+                    <p className="exhibition-gallery-name"><b>Gerald Peters Gallery & Spanierman Modern</b>, Fort Mason Center for Arts & Culture, San Francisco, California</p>
+                    <p className="exhibition-dates">April 27 - 30, 2017</p>
                 </div>
                 <div className="exhibition-container">
-                    <p className="exhibition-year"><b>2004</b></p>
-                    <p className="exhibition-name">Liquid States – Paintings and Drawings</p>
-                    <p className="exhibition-gallery-name"><b>Allan Stone Gallery</b>, New York, New York</p>
-                    <p className="exhibition-dates">May 1 - June 11, 2004</p>
+                    <p className="exhibition-name">Philip Pearlstein: A Legacy of Influence</p>
+                    <p className="exhibition-gallery-name"><b>Arts Council of Princeton</b>, Princeton, New Jersey</p>
+                    <p className="exhibition-dates">January 7 - March 25, 2017</p>
                 </div>
                 <div className="exhibition-container">
-                    <p className="exhibition-year"><b>2009</b></p>
-                    <p className="exhibition-name">Intersections – Paintings</p>
-                    <p className="exhibition-gallery-name"><b>Allan Stone Gallery</b>, New York, New York</p>
-                    <p className="exhibition-dates">February 28 - April 18, 2009</p>
+                    <p className="exhibition-year"><b>2015</b></p>
+                    <p className="exhibition-name">The Depth of The Surface</p>
+                    <p className="exhibition-gallery-name"><b>National Academy of Design Museum</b>, New York, New York</p>
+                    <p className="exhibition-dates">June 4 - August 23, 2015</p>
                 </div>
                 <div className="exhibition-container">
                     <p className="exhibition-year"><b>2000</b></p>
