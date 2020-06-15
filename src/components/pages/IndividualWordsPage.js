b7999867fcaa0c41bc377432871c53376bca8756
Aisha Toulegenova <aishatoulegenova@Aishas-MBP.domain>
2020-06-14 23:37:45 -0700

    words page content

---
+++ src/components/pages/IndividualWordsPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const IndividualWordsPage = () => {
+    return (
+        <div></div>
+    );
+}
+
+export default IndividualWordsPage;
--- src/components/pages/Words.js
+++ src/components/pages/Words.js
@@ -1,9 +1,101 @@
-import React, { Component } from 'react'
+import React, {  } from 'react';
+import wordsTopImage from '../../images/studio/words-page-lorraine.jpg'
 
-class Words extends Component {
-    render() {
-        return <div>Words</div>
-    }
+const Words = () => {
+    return (
+        <div>
+             <div className="image-section">
+                <img className="exhibition-image" src={wordsTopImage} alt="exhibition"/>
+            </div>
+
+            <div className="exhibition-section-type">
+                <p className="exhibition-type-title"><b>Words</b></p>
+                <div className="exhibition-year-group">
+                    <p className="exhibition-year"><b>2020</b></p>
+                    <div className="exhibition-content-right">
+                        <div className="exhibition-container">
+                            <p className="exhibition-name"><b>Catamaran</b>. Literature & Art. Volume 8, Issue 1. </p>
+                            <p className="exhibition-name">(Spring 2020): illus. 60, 71-72.</p>
+                        </div>        
+                    </div>
+                </div>
+                <div className="exhibition-year-group">
+                    <p className="exhibition-year"><b>2019</b></p>
+                    <div className="exhibition-content-right">
+                        <div className="exhibition-container">
+                            <p className="exhibition-name">Balakian, Peter. <b><a href="">"Lorraine Shemesh: Body, Light, Space."</a></b></p>
+                            <p className="exhibition-name">Catalogue Essay, Gerald Peters Gallery, New York, (Sept. 12, 2019): 7-8.</p>
+                        </div>   
+                        <div className="exhibition-container">
+                            <p className="exhibition-name">Zona, Louis, A.<b><a href="">"Introduction."</a></b></p>
+                            <p className="exhibition-name">The Space Between Us Catalogue, Gerald Peters Gallery, New York, (Sept. 12, 2019): 5.</p>
+                        </div>   
+                        <div className="exhibition-container">
+                            <p className="exhibition-name">Boyle, Molly.<b><a target="_blank" href="https://www.nadnowjournal.org/reviews/patterning-en-pointe-lorraine-shemeshs-interstitial-paintings-and-vessels/">"Patterning en Pointe: Lorraine Shemesh's Interstitial Paintings and Vessels."</a></b></p>
+                            <p className="exhibition-name"><b>NAD NOW.</b> (July 17, 2019).</p>
+                        </div>   
+                    </div>   
+                </div>
+                <div className="exhibition-year-group">
+                    <p className="exhibition-year"><b>2017</b></p>
+                    <div className="exhibition-content-right">
+                        <div className="exhibition-container">
+                            <p className="exhibition-name">BPurcell, Janet, <b><a href="">"Fine Arts: Must See Exhibition in Princeton."</a></b></p>
+                            <p className="exhibition-name"><b>The Times of Trenton.</b> (January 18,2017).</p>
+                        </div>   
+                    </div>   
+                </div>
+                <div className="exhibition-year-group">
+                    <p className="exhibition-year"><b>2016</b></p>
+                    <div className="exhibition-content-right">
+                        <div className="exhibition-container">
+                            <p className="exhibition-name">Holmes, Stephanie,<b><a href="">"This Week's Cover Artist: Lorraine Shemesh."</a></b></p>
+                            <p className="exhibition-name"><b>Dan's Papers</b>. (September 2, 2016): Cover, illus., 107.</p>
+                        </div>   
+                        <div className="exhibition-container">
+                            <p className="exhibition-name">Stoppelbein, Annie, "The New York Artist's Painted Pool Series."<b><a href="">"Introduction."</a></b></p>
+                            <p className="exhibition-name"><b>Nashville Arts</b>. (August 2016): Cover, illus., 1-7.</p>
+                        </div>   
+                        <div className="exhibition-container">
+                            <p className="exhibition-name">Bascove, A. <b><a href="">"Inside Out."</a></b>- Review</p>
+                            <p className="exhibition-name">Hamptons Art Hub, (June 3, 2016).</p>
+                        </div>   
+                        <div className="exhibition-container">
+                            <p className="exhibition-name"><b>Catamaran</b>. Literary Reader. Volume 4, Issue 1.<b><a href=""></a></b></p>
+                            <p className="exhibition-name">(Spring 2016): illus. 14-18.</p>
+                        </div>
+                        <div className="exhibition-container">
+                            <p className="exhibition-name">Nash, Steve.<b><a href="">"Suspended: Lorraine Shemesh's Underwater States of Being."</a></b>- Review</p>
+                            <p className="exhibition-name">Catalogue Essay, Gerald Peters Gallery, New York. (May 7, 2016): 7-14.</p>
+                        </div>
+                    </div>   
+                </div>
+                <div className="exhibition-year-group">
+                    <p className="exhibition-year"><b>2011</b></p>
+                    <div className="exhibition-content-right">
+                        <div className="exhibition-container">
+                            <p className="exhibition-name">Shemesh, Lorraine. <b><a href="">"Hands-On Art."</a></b></p>
+                            <p className="exhibition-name"><b>The New Yorker. </b>(August 1, 2011): 5.</p>
+                        </div>   
+                        <div className="exhibition-container">
+                            <p className="exhibition-name">Barrett, Terry. <b><a href="">"Making Art - Form and Meaning."</a></b></p>
+                            <p className="exhibition-name">McGraw-Hill, (2011): plate 3.44, 62-63.</p>
+                        </div>   
+                    </div>   
+                </div>
+                <div className="exhibition-year-group">
+                    <p className="exhibition-year"><b>2010</b></p>
+                    <div className="exhibition-content-right">
+                        <div className="exhibition-container">
+                            <p className="exhibition-name">Brendan, Davis.<b><a href="">"Lorraine Shemesh."</a></b></p>
+                            <p className="exhibition-name"><b>Art Interview ONLINE Magazine.</b> (October, 2010)</p>
+                        </div>   
+                       
+                    </div>   
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Words;
