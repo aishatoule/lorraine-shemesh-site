dc3794f8cc3706831f41f8e0cd84a3437c05c7f4
tillmane <61099006+tillmane@users.noreply.github.com>
2020-06-23 23:03:34 -0700

    Liquid States images

--- src/components/pages/Paint/LiquidStates.js
+++ src/components/pages/Paint/LiquidStates.js
@@ -1,8 +1,91 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { allImagesDesktopOrderBts, allImagesMobileOrderBts } from '../../../imageDataFiles/imageDataLS';
 
 class LiquidStates extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            width: window.innerWidth
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.handleWindowSizeChange);   
+    }
+
+    handleWindowSizeChange = () => {
+        this.setState({
+            width: window.innerWidth
+        });
+    }
+
     render() {
-        return <div>LiquidStates</div>
+
+    let mappedDesktopImages = allImagesDesktopOrderBts.map((imageColumn, i) => {
+        return(
+            <div key={i} className="column">
+            {imageColumn.map((image, index) => {
+                return (
+                    // sends to individual image 
+                    <Link key={index} location={this.props.location} to={{
+                        pathname:"/" + image[0].link, 
+                        state: {
+                            imageInformation: image,
+                            from: this.props.location
+                        }}
+                        
+                    }>
+                        <img key={index} src={image[0].name} className="grid-image" alt="Intersections series"/>
+                    </Link>
+                )                    
+            })}
+            </div>
+        )
+    })
+
+    let mappedMobileImages = allImagesMobileOrderBts.map((imageColumn, i) => {
+        return(
+            <div key={i} className="column">
+            {imageColumn.map((image, index) => {
+                return (
+                    <div className="image-and-details-on-grid">
+                        {/* <Link to={{
+                            pathname:"/" + image.link, 
+                            state: {
+                                imageInformation: image
+                            }}
+                        }> */}
+                        <img key={index} src={image.name} className="grid-image" alt="Intersections series"/>
+                        {/* </Link> */}
+                        <div className="gallery-individual-image-details">
+                            <p>{image.artistName}</p>
+                            <p><span class="italics">{image.title}</span>{image.year !== undefined && ", " + image.year}</p>
+                            <p>{image.dimensions}</p>
+                            <p>{image.privateCollection}</p>
+                        </div>
+                    </div>
+                )                   
+            })}
+            </div>
+        )
+    })
+    const { width } = this.state; 
+    const isMobile = width <= 1000;
+    const mappedImages = (isMobile) 
+    ? 
+    mappedMobileImages
+    :
+    mappedDesktopImages
+
+    return (
+        <div>
+            <div className="image-grid-container">
+                {mappedImages}
+            </div>
+        </div>
+    )
     }
 }
 
---
+++ src/imageDataFiles/imageDataLS.js
@@ -0,0 +1,256 @@
+var zigzag = '../images/paint-ls/zigzag.jpg';
+var zigzagDetail1 = '../images/paint-ls/zigzagDetail1.jpg';
+var handstand = '../images/paint-ls/handstand.jpg';
+var handstandDetail1 = '../images/paint-ls/handstandDetail1.jpg';
+var archway = '../images/paint-ls/archway.jpg';
+var archwayDetail1 = '../images/paint-ls/archwayDetail1.jpg';
+var arch = '../images/paint-ls/arch.jpg';
+var archDetail1 = '../images/paint-ls/archDetail1.jpg';
+var arc = '../images/paint-ls/arc.jpg';
+var eddy = '../images/paint-ls/eddy.jpg';
+var propeller = '../images/paint-ls/propeller.jpg';
+var propellerDetail1 = '../images/paint-ls/propellerDetail1.jpg';
+
+export const allImagesDesktopOrderBts = [
+    [/*COLUMN 1*/
+        [
+            {
+                name: zigzag,
+                artistName: "Lorraine Shemesh",
+                title: "Zig Zag",
+                year: "2003",
+                dimensions: "83 1/2 x 52 3/4 in., o/c",
+                link: "ls1",
+                privateCollection: "Private Collection"
+            },
+            {
+                name: zigzagDetail1,
+                artistName: "Lorraine Shemesh",
+                title: "Zig Zag - Detail",
+                year: "2003",
+                dimensions: "83 1/2 x 52 3/4 in., o/c",
+                link: "ls2",
+                privateCollection: "Private Collection"
+
+            }
+        ],
+        [
+            {
+                name: handstand,
+                artistName: "Lorraine Shemesh",
+                title: "Hand Stand",
+                year: "2003",
+                dimensions: "83 1/4 x 39 1/4 in., o/c",
+                link: "ls3",
+                privateCollection: "Private Collection"
+            },
+            {
+                name: handstandDetail1,
+                artistName: "Lorraine Shemesh",
+                title: "Hand Stand - Detail",
+                year: "2003",
+                dimensions: "83 1/4 x 39 1/4 in., o/c",
+                link: "ls4",
+                privateCollection: "Private Collection"
+            }
+        ],
+        [
+            {    
+                name: archway,
+                artistName: "Lorraine Shemesh",
+                title: "Archway",
+                year: "2001",
+                dimensions: "81 3/8 x 51 5/8 in., o/c",
+                link: "ls5",
+                privateCollection: ""
+            },
+            {
+                name: archDetail1,
+                artistName: "Lorraine Shemesh",
+                title: "Archway - Detail",
+                year: "2001",
+                dimensions: "81 3/8 x 51 5/8 in., o/c",
+                link: "ls6",
+                privateCollection: ""                            
+            }
+        ]
+    ],
+    [/*COLUMN 2*/
+        [
+            {
+                name: arch,
+                artistName: "Lorraine Shemesh",
+                title: "Arch",
+                year: "2001",
+                dimensions: "80 x 57 3/4 in., o/c",
+                link: "ls7",
+                privateCollection: "Collection Novartis Corporation, Basel, Switzerland"
+            },
+            {
+                name: archDetail1,
+                artistName: "Lorraine Shemesh",
+                title: "Arch - Detail",
+                year: "2001",
+                dimensions: "80 x 57 3/4 in., o/c",
+                link: "ls8",
+                privateCollection: "Collection Novartis Corporation, Basel, Switzerland"
+
+            }
+        ],
+        [
+            {
+                name: arc,
+                artistName: "Lorraine Shemesh",
+                title: "Arc",
+                year: "2001",
+                dimensions: "76 7/8 x 62 3/4 in. o/c",
+                link: "ls9",
+                privateCollection: ""
+            }
+        ]
+    ],
+    [/*COLUMN 3*/
+        [
+            {
+                name: eddy,
+                artistName: "Lorraine Shemesh",
+                title: "Eddy",
+                year: "2003",
+                dimensions: "66 1/2 x 66 in.,",
+                link: "ls10",
+                privateCollection: "Private Collection"
+            }
+        ],
+        [
+            {
+                name: propeller,
+                artistName: "Lorraine Shemesh",
+                title: "Propeller",
+                year: "2002",
+                dimensions: "67 1/2 x 71 1/2 in., o/c",
+                link: "ls11",
+                privateCollection: "Private Collection"
+            },
+            {
+                name: propellerDetail1,
+                artistName: "Lorraine Shemesh",
+                title: "Propeller - Detail",
+                year: "2002",
+                dimensions: "67 1/2 x 71 1/2 in., o/c",
+                link: "ls12",
+                privateCollection: "Private Collection"
+            }
+        ]
+    ]          
+]
+
+export const allImagesMobileOrderBts = [
+    [{
+        name: zigzag,
+        artistName: "Lorraine Shemesh",
+        title: "Zig Zag",
+        year: "2003",
+        dimensions: "83 1/2 x 52 3/4 in., o/c",
+        link: "ls1",
+        privateCollection: "Private Collection"
+    },
+    {
+        name: zigzagDetail1,
+        artistName: "Lorraine Shemesh",
+        title: "Zig Zag - Detail",
+        year: "2003",
+        dimensions: "83 1/2 x 52 3/4 in., o/c",
+        link: "ls2",
+        privateCollection: "Private Collection"
+    },
+    {
+        name: handstand,
+        artistName: "Lorraine Shemesh",
+        title: "Hand Stand",
+        year: "2003",
+        dimensions: "83 1/4 x 39 1/4 in., o/c",
+        link: "ls3",
+        privateCollection: "Private Collection"
+    },
+    {
+        name: handstandDetail1,
+        artistName: "Lorraine Shemesh",
+        title: "Hand Stand - Detail",
+        year: "2003",
+        dimensions: "83 1/4 x 39 1/4 in., o/c",
+        link: "ls4",
+        privateCollection: "Private Collection"
+    },
+    {    
+        name: archway,
+        artistName: "Lorraine Shemesh",
+        title: "Archway",
+        year: "2001",
+        dimensions: "81 3/8 x 51 5/8 in., o/c",
+        link: "ls5",
+        privateCollection: ""
+    },
+    {
+        name: archDetail1,
+        artistName: "Lorraine Shemesh",
+        title: "Archway - Detail",
+        year: "2001",
+        dimensions: "81 3/8 x 51 5/8 in., o/c",
+        link: "ls6",
+        privateCollection: ""                            
+    },
+    {
+        name: arch,
+        artistName: "Lorraine Shemesh",
+        title: "Arch",
+        year: "2001",
+        dimensions: "80 x 57 3/4 in., o/c",
+        link: "ls7",
+        privateCollection: "Collection Novartis Corporation, Basel, Switzerland"
+    },
+    {
+        name: archDetail1,
+        artistName: "Lorraine Shemesh",
+        title: "Arch - Detail",
+        year: "2001",
+        dimensions: "80 x 57 3/4 in., o/c",
+        link: "ls8",
+        privateCollection: "Collection Novartis Corporation, Basel, Switzerland"
+    },
+    {
+        name: arc,
+        artistName: "Lorraine Shemesh",
+        title: "Arc",
+        year: "2001",
+        dimensions: "76 7/8 x 62 3/4 in. o/c",
+        link: "ls9",
+        privateCollection: ""
+    },
+    {
+        name: eddy,
+        artistName: "Lorraine Shemesh",
+        title: "Eddy",
+        year: "2003",
+        dimensions: "66 1/2 x 66 in.,",
+        link: "ls10",
+        privateCollection: "Private Collection"
+    },
+    {
+        name: propeller,
+        artistName: "Lorraine Shemesh",
+        title: "Propeller",
+        year: "2002",
+        dimensions: "67 1/2 x 71 1/2 in., o/c",
+        link: "ls11",
+        privateCollection: "Private Collection"
+    },
+    {
+        name: propellerDetail1,
+        artistName: "Lorraine Shemesh",
+        title: "Propeller - Detail",
+        year: "2002",
+        dimensions: "67 1/2 x 71 1/2 in., o/c",
+        link: "ls12",
+        privateCollection: "Private Collection"
+    }]      
+]
