76ec84edfbe45b125934ab260bcdf12a6130482e
davidandrus <davidandrus.com@gmail.com>
2020-09-26 21:08:36 -0700

    updating lockfile and dependencies

--- src/components/pages/Paint/TestBreakingTheSurface.js
+++ src/components/pages/Paint/TestBreakingTheSurface.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { SRLWrapper } from "simple-react-lightbox";
 import { allImagesDesktopOrderBts, allImagesMobileOrderBts } from '../../../imageDataFiles/testImageData';
 
