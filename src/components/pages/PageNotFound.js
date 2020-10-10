a448913c310338a873f1adaa6e0177c2de45665c
tillmane <61099006+tillmane@users.noreply.github.com>
2020-10-10 15:16:15 -0700

    Not Found page style

--- src/components/pages/PageNotFound.js
+++ src/components/pages/PageNotFound.js
@@ -1,5 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-const NotFound = () => <div>Sorry, page not found.</div>
+const NotFound = () => {
+    return (
+        <div>
+            <div className="image-section">
+                <img className="notFound-image" src={'https://lorraineshemeshsiteimgs.s3.us-east-2.amazonaws.com/blank_wall.jpg'} alt="Empty Space"/>
+            </div>
+			<div className="biography-section-type">
+                <p className="biography-type-title"><b>You found a 404 error because the page you were trying to load doesn't exist.</b></p>
+                <p className="biography-type-title"><b>Please try finding what you were looking for using the site menu.</b></p>
+            </div>
+        </div>
+        )
+}
 
 export default NotFound;
