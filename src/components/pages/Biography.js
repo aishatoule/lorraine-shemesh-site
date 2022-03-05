cf285522ef4c39d20d31bb167e8f56508590bea7
tillmane <61099006+tillmane@users.noreply.github.com>
2022-03-05 11:31:54 -0800

    Add Hood Museum to Bio

--- src/components/pages/Biography.js
+++ src/components/pages/Biography.js
@@ -155,6 +155,9 @@ const Biography = () => {
                 <div className="biography-year-group">
                 	<div className="biography-content-left">
                         <div className="biography-container">
+	                        <p className="biography-listing"><b>Hood Museum of Art, Dartmouth</b>, Hanover, New Hampshire.</p>
+	                    </div>
+                        <div className="biography-container">
 	                        <p className="biography-listing"><b>Danforth Museum</b>, Framingham, Massachusetts.</p>
 	                    </div>
 	                    <div className="biography-container">
