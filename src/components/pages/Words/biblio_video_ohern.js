1b6be74394dd53d2e025e5a83f4773dcd4209d57
tillmane <61099006+tillmane@users.noreply.github.com>
2024-03-17 16:37:21 -0700

    Added page turning videos

---
+++ src/components/pages/Words/biblio_video_ohern.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const VideoOHernPages = () => {
+    return (
+    <div>
+        <div className="iframe-container">
+            <iframe title="American Art Collector Pages Video" width="560" height="315" src="https://www.youtube.com/embed/bd8ymAhHdys?autoplay=1&origin=http://lorraineshemesh.com&modestbranding=1&rel=0" allow="fullscreen; autoplay" allowFullScreen></iframe>
+        </div>
+    </div>
+    );
+}
+
+export default VideoOHernPages;
+
