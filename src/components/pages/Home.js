f005ca4b2ff742cd3589e29380b43aff87ee7ee1
tillmane <61099006+tillmane@users.noreply.github.com>
2025-03-29 13:49:15 -0700

    Home page image update

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,8 +5,9 @@ const Home = () => {
         <div className="image-container">
             {/* <img className="individual-image-main" src={'https://lorraineshemeshsiteimgs.s3.us-east-2.amazonaws.com/backbend.jpg'} alt="Backbend - Lorraine Shemesh"/>  */}
             {/* <img className="individual-image-main" src={'https://lorraineshemeshsiteimgs.s3.us-east-2.amazonaws.com/chrysalis.jpg'} alt="Chrysalis - Lorraine Shemesh"/> */}
-            <img className="individual-image-main" src={'https://lorraineshemeshsiteimgs.s3.us-east-2.amazonaws.com/paint-sbu/jigsaw.jpg'} alt="Jigsaw - Lorraine Shemesh"/>
+            {/* <img className="individual-image-main" src={'https://lorraineshemeshsiteimgs.s3.us-east-2.amazonaws.com/paint-sbu/jigsaw.jpg'} alt="Jigsaw - Lorraine Shemesh"/> */}
             {/* <img className="individual-image-main" src={'https://lorraineshemeshsiteimgs.s3.us-east-2.amazonaws.com/paint-sbu/timebeing.jpg'} alt="TimeBeing - Lorraine Shemesh"/> */}
+            <img className="individual-image-main" src={'https://lorraineshemeshsiteimgs.s3.us-east-2.amazonaws.com/studio/studio_20250329.jpg'} alt="Lorraine Shemesh in the studio"/>
         </div>
     )
 }
