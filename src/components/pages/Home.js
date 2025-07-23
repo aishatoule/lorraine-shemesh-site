fe02109613c9cdbf887531711a6212ccb2a241a6
tillmane <61099006+tillmane@users.noreply.github.com>
2025-07-23 08:29:41 -0700

    home image update

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,7 +7,10 @@ const Home = () => {
             {/* 2 <img className="individual-image-main" src={'https://lorraineshemeshsiteimgs.s3.us-east-2.amazonaws.com/chrysalis.jpg'} alt="Chrysalis - Lorraine Shemesh"/> */}
             {/* 3 <img className="individual-image-main" src={'https://lorraineshemeshsiteimgs.s3.us-east-2.amazonaws.com/studio/studio_20250329.jpg'} alt="Lorraine Shemesh in the studio"/> */}
             {/* 4 <img className="individual-image-main" src={'https://lorraineshemeshsiteimgs.s3.us-east-2.amazonaws.com/aroundthebend.jpg'} alt="Around the Bend - Lorraine Shemesh"/> */}
-            { <img className="individual-image-main" src={'https://lorraineshemeshsiteimgs.s3.us-east-2.amazonaws.com/paint-sbu/timebeing.jpg'} alt="TimeBeing - Lorraine Shemesh"/> }
+
+            {<img className="individual-image-main" src={'https://lorraineshemeshsiteimgs.s3.us-east-2.amazonaws.com/torque.jpg'} alt="Torque - Lorraine Shemesh"/>}
+
+            {/* <img className="individual-image-main" src={'https://lorraineshemeshsiteimgs.s3.us-east-2.amazonaws.com/paint-sbu/timebeing.jpg'} alt="TimeBeing - Lorraine Shemesh"/> */}
             {/* <img className="individual-image-main" src={'https://lorraineshemeshsiteimgs.s3.us-east-2.amazonaws.com/paint-sbu/jigsaw.jpg'} alt="Jigsaw - Lorraine Shemesh"/> */}
         </div>
     )
